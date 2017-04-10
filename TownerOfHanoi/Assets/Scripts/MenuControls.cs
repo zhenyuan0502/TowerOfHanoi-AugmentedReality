636dbbcde5b8fda0008c8f84a9b2a0a00f0303c7
NguyenLuu <nguyenluu@mapmaker.asia>
2017-04-11 03:32:51 +0700

    First commit + Integrating Menu screen

---
+++ TownerOfHanoi/Assets/Scripts/MenuControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void StartButton()
+    {
+        Application.LoadLevel("GamePlay");
+    }
+    public void OptionsButton()
+    {
+        Application.LoadLevel("Options");
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
