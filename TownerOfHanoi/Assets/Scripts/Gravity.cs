6c454342d64884c53aebf6f13f4bc09a7fb31713
NguyenLuu <nguyenluu@mapmaker.asia>
2017-04-25 03:14:27 +0700

    Add + fix gravity

--- TownerOfHanoi/Assets/Scripts/Gravity.cs
+++ TownerOfHanoi/Assets/Scripts/Gravity.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
-		
+		transform.position = new Vector3(0, 0, 0.2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (trans.position.y > 0) {
+
+		} else {
+			trans.position.y = 0;
+		}
 	}
 }
