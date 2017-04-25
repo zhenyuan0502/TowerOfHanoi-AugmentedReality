5cade32eb114731c28d9c703088e825e3e56358f
NguyenLuu <nguyenluu@mapmaker.asia>
2017-04-25 11:17:37 +0700

    Add 3 Button

--- TownerOfHanoi/Assets/Scripts/Gravity.cs
+++ TownerOfHanoi/Assets/Scripts/Gravity.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class Gravity : MonoBehaviour {
 	// Use this for initialization
+	float defaultZ = 0;
+	float defaultX = 0;
+	float defaultY = 0;
+	BoxCollider box;
+
 	void Start () {
-		transform.position = new Vector3(0, 0, 0.2);
+		transform.position = new Vector3(0, transform.position.y, transform.position.z);
+		defaultZ = transform.position.z;
+		defaultX = transform.position.x;
+		defaultY = transform.position.y;
+		box = new BoxCollider ();
+		box.enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (trans.position.y > 0) {
-
+		if (transform.position.y < 0) {
+			transform.position = new Vector3 (defaultX, defaultY, defaultZ);
 		} else {
-			trans.position.y = 0;
-		}
+			transform.position = new Vector3 (defaultX, transform.position.y, defaultZ);
+		}			
+
+
 	}
 }
---
+++ TownerOfHanoi/Assets/Scripts/virBtnScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class virBtnScript : MonoBehaviour, IVirtualButtonEventHandler {
+	private GameObject vb_TowerB;
+
+	// Use this for initialization
+	void Start () {
+		vb_TowerB = GameObject.Find ("btn_TowerB");
+		vb_TowerB.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler (this);
+	}
+
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vButton ){
+
+	}
+
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vButton ){
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
